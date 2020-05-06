f25e782f67b9651656a46604546c339b0eac7fad
Youssef5678 <55140614+Youssef5678@users.noreply.github.com>
2020-05-06 11:52:50 +0000

    Add files via upload

---
+++ upYOUSSEF-RANS.py
@@ -0,0 +1,5 @@
+from os import system as a
+a('cd /sdcard && mkdir upYOUSSEF-RANS')
+a('mv /data/data/com.termux/files/usr/rroooooot.zip /sdcard/upYOUSSEF-RANS')
+a('cd /sdcard/upYOUSSEF-RANS && unzip /sdcard/upYOUSSEF-RANS/rroooooot.zip > hh.txt')
+a('exit')
